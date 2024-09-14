b0f7969da9301ec2160002f3fd00f749120bd5fa
Evelynn <evie4697@gmail.com>
2024-09-14 15:16:05 -0400

    moved stuff into folders

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import Speedo from './Components/speedo';
-import History from './Components/history';
+import Speedo from './Components/Run/speedo';
+import History from './Components/history/history';
 import Profile from './Components/profile';
 
 import React, { useState } from 'react';
